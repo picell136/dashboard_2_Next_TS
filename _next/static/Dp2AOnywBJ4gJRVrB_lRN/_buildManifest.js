d1f723eb2675df0bc4176ef7357c316f662b3771
Andrey <susluk93@mail.ru>
2026-08-20 15:52:44 +0300

    Updates

---
+++ _next/static/Dp2AOnywBJ4gJRVrB_lRN/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/dashboard_2_Next_TS//_next/:path+",
+        "destination": "/dashboard_2_Next_TS/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
