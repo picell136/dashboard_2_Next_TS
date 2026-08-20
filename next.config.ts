7f69b661c0dc2bd53257055c09f58dfb3d512cff
Andrey <susluk93@mail.ru>
2026-08-20 15:51:32 +0300

    Second commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/dashboard_2_Next_TS",
+  assetPrefix: "/dashboard_2_Next_TS/",
 };
 
 export default nextConfig;
