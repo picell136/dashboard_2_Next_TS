79b03aab75518f773d6d2748b381bafc7f43554d
Andrey <susluk93@mail.ru>
2026-08-20 16:11:21 +0300

    Edited next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/dashboard_2_Next_TS",
-  assetPrefix: "/dashboard_2_Next_TS/",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
